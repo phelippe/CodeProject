e02d9d12b12e156bef4036ed33efa120406d51da
Phelippe <phelippematte@gmail.com>
2015-09-05 18:57:09 -0300

    Fim de "Começão com os CRUDS"

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,27 +1,67 @@
 var app = angular.module('app', ['ngRoute', 'angular-oauth2', 'app.controllers']);
 
-angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
+angular.module('app.controllers', ['ngMessages', 'angular-oauth2', 'app.services']);
+angular.module('app.services', ['ngResource']);
 
-app.config(['$routeProvider', 'OAuthProvider', function ($routeProvider, OAuthProvider) {
-    $routeProvider
-        .when('/login', {
-            templateUrl: 'build/views/login.html',
-            controller: 'LoginController',
-        })
-        .when('/home', {
-            templateUrl: 'build/views/home.html',
-            controller: 'HomeController',
-        });
-    OAuthProvider.configure({
+app.provider('appConfig', function () {
+    var config = {
         baseUrl: 'http://localhost:8000',
-        clientId: 'appid1',
-        clientSecret: 'secret',
-        grantPath: 'oauth/access_token',
-    });
-}]);
+    }
 
-app.run(['$rootScope', '$window', 'OAuth', function($rootScope, $window, OAuth) {
-    $rootScope.$on('oauth:error', function(event, rejection) {
+    return {
+        config: config,
+        $get: function () {
+            return config;
+        }
+    };
+});
+
+app.config([
+    '$routeProvider', 'OAuthProvider', 'OAuthTokenProvider', 'appConfigProvider',
+    function ($routeProvider, OAuthProvider, OAuthTokenProvider, appConfigProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'build/views/login.html',
+                controller: 'LoginController',
+            })
+            .when('/home', {
+                templateUrl: 'build/views/home.html',
+                controller: 'HomeController',
+            })
+            .when('/clientes', {
+                templateUrl: 'build/views/client/list.html',
+                controller: 'ClientListController',
+            })
+            .when('/clientes/new', {
+                templateUrl: 'build/views/client/new.html',
+                controller: 'ClientNewController',
+            })
+            .when('/clientes/:id/edit', {
+                templateUrl: 'build/views/client/edit.html',
+                controller: 'ClientEditController',
+            })
+            .when('/clientes/:id/delete', {
+                templateUrl: 'build/views/client/delete.html',
+                controller: 'ClientDeleteController',
+            });
+
+        OAuthProvider.configure({
+            baseUrl: appConfigProvider.config.baseUrl,
+            clientId: 'appid1',
+            clientSecret: 'secret',
+            grantPath: 'oauth/access_token',
+        });
+
+        OAuthTokenProvider.configure({
+            name: 'token',
+            options: {
+                secure: false,
+            },
+        });
+    }]);
+
+app.run(['$rootScope', '$window', 'OAuth', function ($rootScope, $window, OAuth) {
+    $rootScope.$on('oauth:error', function (event, rejection) {
         // Ignore `invalid_grant` error - should be catched on `LoginController`.
         if ('invalid_grant' === rejection.data.error) {
             return;
